681e31491388d2b98cade26a9146dfcf0517c045
Biq <oEBnZ8hr5Yyj@gmx.com>
2021-11-17 06:38:36 -0500

    test bitboard functions

--- vice.c
+++ vice.c
@@ -11,5 +11,11 @@ int main() {
     playBitBoard |= (1ULL << SQ64(D3));
     playBitBoard |= (1ULL << SQ64(D4));
 
+    PrintBitBoard(playBitBoard);
+
+    int count = CNT(playBitBoard);
+
+    printf("Count: %d\n", count);
+
     return 0;
 }
