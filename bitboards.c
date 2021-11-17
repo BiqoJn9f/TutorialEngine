b0bb89b019bcdfcbdd15c0eac1309c5938462aa0
Biq <oEBnZ8hr5Yyj@gmx.com>
2021-11-17 06:06:20 -0500

    finish PrintBitBoard function

--- bitboards.c
+++ bitboards.c
@@ -9,4 +9,17 @@ void PrintBitBoard(U64 bb) {
     int sq = 0;
     int sq64 =0;
 
+    printf("\n");
+    for(rank = RANK_8; rank >= RANK_1; --rank) {
+        for(file = FILE_A; file <= FILE_H; ++file) {
+            sq = FR2SQ(file,rank);
+            sq64 = SQ64(sq);
+            if((shiftMe << sq64) && bb)
+                printf("X");
+            else
+                printf("-");
+        }
+        printf("\n");
+    }
+    printf("\n\n");
 }
