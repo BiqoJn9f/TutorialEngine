76314d389377c5c2c9fd9fde769eb89d28fe053d
Biq <oEBnZ8hr5Yyj@gmx.com>
2021-11-17 06:51:48 -0500

    complete InitBitMasks function

--- init.c
+++ init.c
@@ -10,6 +10,16 @@ U64 ClearMask[64];
 
 void InitBitMasks() {
     int index = 0;
+
+    for(index = 0; index < 64; index++) {
+        SetMask[index] = 0ULL;
+        ClearMask[index] = 0ULL;
+    }
+
+    for(index = 0; index < 64; index++) {
+        SetMask[index] |= (1ULL << index);
+        ClearMask[index] = ~SetMask[index];
+    }
 }
 
 void InitSq120To64() {
